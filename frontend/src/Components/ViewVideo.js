5e655e30c89bc01ac5f7c01c8c23583257eb2377
AnanyaGanji <139428449+AnanyaGanji@users.noreply.github.com>
2025-03-05 14:04:08 +0530

    Update ViewVideo.js

--- frontend/src/Components/ViewVideo.js
+++ frontend/src/Components/ViewVideo.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./ViewVideo.css";
 
 const ViewVideo = () => {
+  const navigate = useNavigate();
+
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const videoPath = queryParams.get("video");
@@ -9,10 +13,11 @@ const ViewVideo = () => {
   return (
     <div className="video-page">
       <h2>Video Player</h2>
-      <video width="600" controls>
+      <video width="500" height="500" controls>
         <source src={`http://localhost:5000${videoPath}`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <button onClick={() => navigate("/videos")}>Back</button>
     </div>
   );
 };
